46fd74407efd9bd28c607240662d8f68178266e0
guzi <guzi@guzideMacBook-Pro.local>
2021-07-12 23:02:52 +0800

    init 0.0.1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+defineProps({
+  msg: { type: String, default: 'Hello vue3' }
+})
+</script>
